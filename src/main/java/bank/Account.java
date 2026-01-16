26566188cb606d69232493c7b8ac255ae6d1c0d6
Thomas Meder <10582949+TomMeHo@users.noreply.github.com>
2026-01-16 14:48:07 +0000

    account class

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,39 @@
+package bank;
+
+public class Account {
+
+  private int id;
+  private String accountType;
+  private double balance;
+
+  public Account(int id, String accountType, double balance) {
+    setId(id);
+    setAccountType(accountType);
+    setBalance(balance);
+  }
+
+  public String getAccountType(){
+    return this.accountType;
+  }
+
+  public void setAccountType(String accountType) {
+    this.accountType = accountType;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+}
